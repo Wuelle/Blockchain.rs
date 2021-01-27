5b0967f16fb3230c6def2034efd22c0319436595
Wuelle <simon.wuelker@arcor.de>
2021-01-27 21:40:11 +0100

    stuck at lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use rsa::{PublicKey, RSAPrivateKey, RSAPublicKey, PaddingScheme};
+use rsa::{RSAPrivateKey, RSAPublicKey, PaddingScheme};
 use rand::rngs::OsRng;
 use std::collections::hash_map::DefaultHasher;
 use std::hash::{Hash, Hasher};
-use rand::{distributions::Alphanumeric, Rng}; 
+use rand::{distributions::Alphanumeric, Rng};
+use byteorder::{LittleEndian, WriteBytesExt};
+
 struct Trader{
     public_key: RSAPublicKey,
     private_key: RSAPrivateKey,
@@ -10,15 +12,16 @@ struct Trader{
 }
 
 #[derive(Hash)]
-struct Transaction{
-    sender: Trader,
-    receiver: Trader,
+struct Transaction<'a>{
+    sender: &'a Trader,
+    receiver: &'a Trader,
     amount: i32,
 }
 
-struct SignedTransaction{
-    transaction: Transaction,
-    signature: String,
+#[derive(Hash)]
+struct SignedTransaction<'c>{
+    transaction: Transaction<'c>,
+    signature: Vec<u8>,
 }
 
 impl Trader{
@@ -40,27 +43,49 @@ impl Trader{
             id: id,
         }
     }
-//    /// Send currency to another Trader
-//    pub fn send() -> Transaction{
-//        
-//    }
 }
 impl Hash for Trader{
     fn hash<H: Hasher>(&self, state: &mut H){
         self.id.hash(state);
     }
 }
-fn calculate_hash<T: Hash>(t: &T) -> u64{
+
+impl SignedTransaction<'_>{
+    pub fn new<'b>(sender: &'b Trader, receiver: &'b Trader, amount: i32) -> Self{
+        let t = Transaction{
+            sender: sender,
+            receiver: receiver,
+            amount: amount,
+        };
+        // Sign the hashed transaction
+        let hashed = calculate_hash(&t);
+        let padding = PaddingScheme::new_pkcs1v15_encrypt(); 
+        let s = sender.private_key.sign(padding, &hashed).unwrap();
+
+        SignedTransaction{
+            transaction: t,
+            signature: s
+        }
+    }
+}
+fn calculate_hash<T: Hash>(t: &T) -> [u8; 8]{
     let mut s = DefaultHasher::new();
     t.hash(&mut s);
-    s.finish()
+    let hash = s.finish();
+    //let mut bs = [0u8; 64];
+    //bs.as_mut()
+    //    .write_u64::<LittleEndian>(hash)
+    //    .expect("Unable to convert hash to bytes");
+    //return bs
+    return hash.to_ne_bytes()
 }
 
-fn main() {
-    let padding = PaddingScheme::new_pkcs1v15_encrypt();
+//static t1: Trader = Trader::new();
+//static t2: Trader = Trader::new();
 
-    let t1: Trader = Trader::new();
-    let t2: Trader = Trader::new();
-    println!("{:?}", t1.id);
-    assert!(calculate_hash(&t1) != calculate_hash(&t2));
+fn main() {
+    let t1 = Trader::new();
+    let t2 = Trader::new();
+    let sta = SignedTransaction::new(&t1, &t2, 100);
+    println!("{}", t1.id);
 }
