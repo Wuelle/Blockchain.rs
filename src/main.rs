5dc4b9498e839a497968fb32158ee8b0842e5484
Wuelle <simon.wuelker@arcor.de>
2021-03-12 17:44:01 +0100

    sharing memory for trader/miner pair (still p2p though!)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use blockchain::trader::Trader;
 use blockchain::transaction::Transaction;
 use blockchain::miner::Miner;
-use log::{info, trace, warn};
 use simple_logger::SimpleLogger;
 
 fn main() {
@@ -11,23 +10,22 @@ fn main() {
     // Create core entities
     let t1 = Trader::new();
     let t2 = Trader::new();
-    let m1 = Miner::new(&t1); // Miner linked to t1
+    let m1 = t1.spawn_miner_thread();
+
+    t1.register_miner(&m1);
+    t2.register_miner(&m1);
+
+    //let m1 = Miner::new(&t1); // Miner linked to t1
 
     // Link Miners/Traders to create P2P network
-    t1.add_miner(&m1);
-    t2.add_miner(&m1);
+    //t1.add_miner(&m1);
+    //t2.add_miner(&m1);
 
     // 'me' is synonymous to 'self' since 'self' can't be assigned
-    let target_key = t2.public_key.clone();
-    t1.execute(move|me| {
-        let t = Transaction::new(me.public_key.clone(), target_key, 1.0);
-        let st = me.sign(t);
+    let t = Transaction::new(t1.public_key.clone(), t2.public_key.clone(), 1.0);
+    let st = t1.sign(t);
+    t1.broadcast(&st);
 
-        // Broadcast transaction to miners
-        for miner in &me.known_miners {
-            miner.send(st.clone()).unwrap();
-        }
-    });
 
     // Wait for the user to stop execution (Ctrl+C)
     loop{}
